7eb3ee3e62d6121baf736c58cbce48ae368cecc6
Colton Padden <colton@dagsterlabs.com>
2024-05-29 21:30:49 -0400

    add plot

--- nyc-vehicle-collisions.R
+++ nyc-vehicle-collisions.R
@@ -3,10 +3,8 @@ install.packages("tidyverse")
 library(tidyverse)
 
 # https://catalog.data.gov/dataset/motor-vehicle-collisions-crashes
-
 VEHICLE_COLLISIONS_URL <- "https://data.cityofnewyork.us/api/views/h9gi-nx95/rows.csv"
 VEHICLE_COLLISIONS_CACHE_PATH <- "data/nyc-collisions.csv"
-
 
 if (file.exists(VEHICLE_COLLISIONS_CACHE_PATH)) {
   df <- read_csv(VEHICLE_COLLISIONS_CACHE_PATH)
@@ -16,21 +14,20 @@ if (file.exists(VEHICLE_COLLISIONS_CACHE_PATH)) {
   write.csv(df, file = VEHICLE_COLLISIONS_CACHE_PATH, row.names = TRUE)
 }
 
-
 colnames(df) <- gsub(" ", "_", colnames(df))
 
-df %>%
+df_collision_counts <- df %>%
   filter(!is.na(BOROUGH)) %>%
   mutate(CRASH_YEAR = format(strptime(CRASH_DATE, "%m/%d/%Y"), "%Y")) %>%
   group_by(BOROUGH, CRASH_YEAR) %>%
   count(name = "CRASH_COUNT")
 
+g <- ggplot(df_collision_counts, aes(x=CRASH_YEAR, y=CRASH_COUNT, group=BOROUGH)) +
+  ggtitle("NYC Vehicle Collisions by Borough") +
+  xlab("Year") +
+  ylab("Number of Collisions") +
+  geom_line() +
+  facet_wrap(~BOROUGH, ncol = 1)  +
+  stat_smooth(method="lm", fullrange=TRUE, color='purple', linetype='dashed', size=0.4)
 
-
-
-# time of day
-
-# filter where borough is not NA
-#
-
-# cache data to file so that subsequent runs aren't pulled from data.gov
+ggsave('nyc-vehicle-collisions-by-borough.png', plot=g, dpi=600)
